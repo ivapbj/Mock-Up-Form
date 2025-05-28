f4a5b23348a7071e3805e69a7decf5c537bd633e
Ivana Carrillo <ivanae.carrillo@gmail.com>
2025-05-27 22:26:06 -0400

    changes transporter to transport in api

--- api/submit-form.js
+++ api/submit-form.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
     }
 
     // Create transporter
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
